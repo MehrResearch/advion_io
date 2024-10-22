9056ed0f1ac1e5b2b837211b4f0a11d457e14fd3
Hessam Mehr <Hessam.Mehr@glasgow.ac.uk>
2024-10-22 13:56:35 +0100

    Clean up conversion from notebook and bundle dlls.

--- src/advion_io/advion_data.py
+++ src/advion_io/advion_data.py
@@ -1,13 +1,10 @@
-#%%
 import ctypes
-from enum import IntEnum
+
 import numpy as np
 
-from matplotlib import pyplot as plt
+from .constants import NAMES, AdvionDataErrorCode
 
-from .constants import AdvionDataErrorCode, NAMES
-
-lib = ctypes.cdll.LoadLibrary('../AdvionAPI.6.4.14.1.VS15x64/Release/AdvionData.dll')
+lib = ctypes.cdll.LoadLibrary("./lib/AdvionData.dll")
 
 bindings = {}
 
@@ -16,22 +13,24 @@ for demangled, (mangled, arg_types, res_type) in NAMES.items():
     bindings[demangled].argtypes = arg_types
     bindings[demangled].restype = res_type
 
-# %%
+
 class AdvionData:
     def __init__(self, path, debug_output, decode_spectra):
         self.handle = ctypes.create_string_buffer(32)
-        bindings['DataReader'](ctypes.byref(self.handle), path, debug_output, decode_spectra)
+        bindings["DataReader"](
+            ctypes.byref(self.handle), path, debug_output, decode_spectra
+        )
 
     def get_num_masses(self):
-        return bindings['DataReader::getNumMasses'](ctypes.byref(self.handle))
+        return bindings["DataReader::getNumMasses"](ctypes.byref(self.handle))
 
     def get_num_spectra(self):
-        return bindings['DataReader::getNumSpectra'](ctypes.byref(self.handle))
-    
+        return bindings["DataReader::getNumSpectra"](ctypes.byref(self.handle))
+
     def get_masses(self):
         num_masses = self.get_num_masses()
         buffer = (ctypes.c_float * num_masses)()
-        err_no = bindings['DataReader::getMasses'](ctypes.byref(self.handle), buffer)
+        err_no = bindings["DataReader::getMasses"](ctypes.byref(self.handle), buffer)
         if err_no != 0:
             err_code = AdvionDataErrorCode(err_no)
             raise IOError(err_code)
@@ -40,24 +39,29 @@ class AdvionData:
     def get_retention_times(self):
         num_spectra = self.get_num_spectra()
         buffer = (ctypes.c_float * num_spectra)()
-        err_no = bindings['DataReader::getRetentionTimes'](ctypes.byref(self.handle), buffer)
+        err_no = bindings["DataReader::getRetentionTimes"](
+            ctypes.byref(self.handle), buffer
+        )
         if err_no != 0:
             err_code = AdvionDataErrorCode(err_no)
             raise IOError(err_code)
         return np.ctypeslib.as_array(buffer)
-    
+
     def get_TIC(self, index):
-        return bindings['DataReader::getTIC'](ctypes.byref(self.handle), index)
+        return bindings["DataReader::getTIC"](ctypes.byref(self.handle), index)
 
     def get_spectrum(self, index):
         max_spectrum = self.get_num_spectra() - 1
         if index >= max_spectrum:
-            raise IndexError(f'Requested index {index} needs to be smaller than {max_spectrum}.')
+            raise IndexError(
+                f"Requested index {index} needs to be smaller than {max_spectrum}."
+            )
         num_masses = self.get_num_masses()
         buffer = (ctypes.c_float * num_masses)()
-        err_no = bindings['DataReader::getSpectrum'](ctypes.byref(self.handle), index, buffer)
+        err_no = bindings["DataReader::getSpectrum"](
+            ctypes.byref(self.handle), index, buffer
+        )
         if err_no != 0:
             err_code = AdvionDataErrorCode(err_no)
             raise IOError(err_code)
         return np.ctypeslib.as_array(buffer)
-    
